72def81a7250099511f4c9bd417f9aff7c64a07f
Prashant Baditya <baditya.prashant@gmail.com>
2024-06-20 08:55:02 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prashant
+          Prashant Baditya
         </p>
         <a
           className="App-link"
